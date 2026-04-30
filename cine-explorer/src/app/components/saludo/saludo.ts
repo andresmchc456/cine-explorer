ad63867e8ecbc16f95a5476fecbe11bf647613ec
Andrés Meléndez <andresmchc456@gmail.com>
2026-04-30 12:24:59 -0500

    parte 2

---
+++ cine-explorer/src/app/components/saludo/saludo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saludo',
+  imports: [],
+  template: ` <p>saludo works!</p> `,
+  styles: ``,
+})
+export class Saludo {}
