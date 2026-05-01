0bd89eba2e01a95936f6eaeb5124a1b5c7eb4b7d
andresmchc456 <andresmchc456@gmail.com>
2026-04-30 19:54:33 -0500

    parte 3 de la guia angular

--- cine-explorer/src/app/components/movie-card/movie-card.ts
+++ cine-explorer/src/app/components/movie-card/movie-card.ts
@@ -1,7 +1,7 @@
 // movie-card.component.ts
 // Componente que muestra una tarjeta con la información de una película
-import { Component } from '@angular/core';
-// Importar la interfaz Movie que creamos en el capítulo 1
+import { Component, input, output } from '@angular/core';
+
 import { Movie } from '../../models/movie';
 
 @Component({
@@ -14,14 +14,30 @@ import { Movie } from '../../models/movie';
 export class MovieCardComponent {
   // Por ahora usamos datos de ejemplo hardcodeados
   // En el próximo capítulo recibiremos los datos del componente padre con @Input
-  pelicula: Movie = {
-    id: 550,
-    title: 'Fight Club',
-    overview: 'Un oficinista insomne y un fabricante de jabón forman un club de pelea clandestino que evoluciona hacia algo mucho más peligroso.',
-    poster_path: '/pB8BM7pdSp6B6Ih7QI4S2t0POoD.jpg',
-    backdrop_path: '/hZkgoQYus5dXo3H8T7Uef6DNknx.jpg',
-    vote_average: 8.4,
-    release_date: '1999-10-15',
-    genre_ids: [18, 53]
-  };
+  // pelicula: Movie = {
+  //   id: 550,
+  //   title: 'Fight Club',
+  //   overview: 'Un oficinista insomne y un fabricante de jabón forman un club de pelea clandestino que evoluciona hacia algo mucho más peligroso.',
+  //   poster_path: '/pB8BM7pdSp6B6Ih7QI4S2t0POoD.jpg',
+  //   backdrop_path: '/hZkgoQYus5dXo3H8T7Uef6DNknx.jpg',
+  //   vote_average: 8.4,
+  //   release_date: '1999-10-15',
+  //   genre_ids: [18, 53]
+  // };
+   movie = input.required<Movie>();
+
+  // input<boolean>() con valor por defecto false (no es obligatorio pasarlo)
+  esFavorita = input<boolean>(false);
+
+  
+  toggleFavorito = output<Movie>();
+
+  
+  onToggleFavorito(): void {
+    this.toggleFavorito.emit(this.movie());
+  }
 }
+
+
+
+
